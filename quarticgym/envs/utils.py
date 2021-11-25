f4d8e22c3e2e8a9be2c1bf753cf2b975d3ae9e82
Mohan Zhang <morgan.zhang@mail.utoronto.ca>
2021-11-25 16:10:30 -0500

    added data generation

--- quarticgym/envs/utils.py
+++ quarticgym/envs/utils.py
@@ -1,1 +1,18 @@
 from mzutils import parent_dir_and_name, get_things_in_loc, normalize_spaces, denormalize_spaces, mkdir_p
+from torch.utils.data import Dataset
+
+class TorchDatasetFromD4RL(Dataset):
+    def __init__(self, dataset_d4rl) -> None:
+        import d3rlpy
+        # super().__init__()
+        """
+        dataset_d4rl should be returned by lbd_data_obj.get_dataset()
+        """
+        self.dataset = d3rlpy.dataset.MDPDataset(dataset_d4rl['observations'], dataset_d4rl['actions'], dataset_d4rl['rewards'], dataset_d4rl['terminals'])
+        
+    def __len__(self) -> int:
+        return self.dataset.__len__()
+
+    def __getitem__(self, idx):
+        episode =self.dataset.__getitem__(idx)
+        return {'observations': episode.observations, 'actions': episode.actions, 'rewards': episode.rewards}
